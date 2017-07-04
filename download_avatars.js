6b9334f63beaea0ba9cbcae4bb5337ac683ace92
Kai19 <ngyankai@hotmail.com>
2017-07-04 01:46:34 +0000

    made requestURL options function, made function to request body of url and console log list of contributors

--- download_avatars.js
+++ download_avatars.js
@@ -3,13 +3,37 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
-var GITHUB_USER =  "";
-var GITHUB_TOKEN = "";
+const GITHUB_USER =  process.argv[2];
+const GITHUB_TOKEN = process.argv[3];
+
+function getRequestOptions(path) {
+  return {
+    url: path,
+    headers: {
+      'User-Agent': "GitHub Avatar Downloader - Student Project"
+    },
+    qs: {
+      access_token: GITHUB_TOKEN
+    }
+  };
+}
+
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
+
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+
+  request(getRequestOptions(requestURL), function(err, response, body) {
+    if (err){
+    console.log("Failed to request URL");
+    }else{
+      const data = JSON.parse(body);
+      console.log(data);
+    }
+  });
 }
 
 
